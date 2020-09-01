854319ec70c711b79f88f5ecc47643f536a87a98
Aspelic <as182219@ohio.edu>
2020-09-01 17:13:32 -0400

    Hello Git!

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Adam Spelic
+ *        Date: 9/01/2020
+ * Description: git-lab-program code in git-lab-2 repo
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
